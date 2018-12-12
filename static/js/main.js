a701327feff49ba1268cacff429d62aa49a39fe9
Kenny Pham <kennypham@kennys-mbp-2.attlocal.net>
2018-12-12 01:15:28 -0800

    Initial commit

---
+++ static/js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    $("#hello-world").hide();
+    $("#kenny").hide();
+    $("#kenny").show(1500);
+    $("#hello-world").fadeIn(4500);
+
+    var scrollLink = $('.scroll');
+    
+    // Smooth scrolling
+    scrollLink.click(function(e) {
+      e.preventDefault();
+      $('body, html').animate({ //animate is looking for a css properties
+        scrollTop: $(this.hash).offset().top - 80 //scrollTop looks at the location of the scrollbar. 
+      }, 1000 );
+    });
+    
+    // Active link highlighting
+    $(window).scroll(function() {
+      var scrollbarLocation = $(this).scrollTop(); //measures how far and where you are within the window
+      scrollLink.each(function() {
+        var sectionOffset = $(this.hash).offset().top;
+        if ( sectionOffset <= scrollbarLocation ) {
+          $(this).parent().addClass('active'); //parent = active of which is on the first link
+          $(this).parent().siblings().removeClass('active');
+        }
+      })
+    })
+  })
+  
+  
