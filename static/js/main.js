310d46de33f18694b5b62b715998a636cead66f6
Kenny Pham <kennypham@kennys-mbp-2.attlocal.net>
2020-08-02 22:42:09 -0700

    new smooth scroll

--- static/js/main.js
+++ static/js/main.js
@@ -1,34 +1,34 @@
 $(document).ready(function () {
   $("#hello").hide();
   $("#kenny").hide();
-  $("#kenny").show(900);
-  $("#hello").fadeIn(3700);
+  $("#kenny").show(1500);
+  $("#hello").fadeIn(2800);
+  $(".resume-btn").hide();
+  $(".resume-btn").fadeIn(2000);
 
-  var scrollLink = $('.scroll');
+  let scrollLink = $(".scroll");
 
   // Smooth scrolling
   scrollLink.click(function (e) {
-    e.preventDefault();
-    $('body, html').animate({ //animate is looking for a css properties
-      scrollTop: $(this.hash).offset().top - 80 //scrollTop looks at the location of the scrollbar. 
-      // scrollTop: $(this.hash).offset().top - 58 //scrollTop looks at the location of the scrollbar. 
-    }, 1000);
+    if (this.hash !== "") {
+      e.preventDefault();
+      const hash = this.hash;
+      $("body, html").animate({
+          //animate is looking for a css properties
+          scrollTop: $(hash).offset().top - 63.5, //scrollTop looks at the location of the scrollbar.
+        },
+        800
+      );
+    }
   });
 
-  // Active link highlighting
-  $(window).scroll(function () {
-    var scrollbarLocation = $(this).scrollTop(); //measures how far and where you are within the window
-    scrollLink.each(function () {
-      var sectionOffset = $(this.hash).offset().top;
-      if (sectionOffset <= scrollbarLocation) {
-        $(this).parent().addClass('active'); //parent = active of which is on the first link
-        $(this).parent().siblings().removeClass('active');
-      }
-    })
-  })
+  // Collapses menu on click
+  $(".nav-item").on("click", function () {
+    $(".navbar-collapse").collapse("hide");
+  });
 
   //Reset Carousel
-  $('.project-image').click(function () {
-    $('.carousel').carousel(0);
+  $(".project-image").click(function () {
+    $(".carousel").carousel(0);
   });
-})
+});
